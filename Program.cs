f516f21b4119069503b6ac3d7e461525def2e01b
luca-gif <luca.89m@gmail.com>
2022-09-17 14:53:20 +0200

    Esercizio 1

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+//void StampaArray(int[] array): che preso un array di numeri interi e
+//stampa a video il contenuto dell’array in questa forma “[elemento 1, elemento 2, elemento 3, ...]“.
 
+int[] numbers = {1,2,3,4,5};
+
+
+void StampaArray(int[] array)
+{
+    Console.Write("[");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + ",");
+    }
+
+    Console.Write("]");
+}
+
+StampaArray(numbers);
