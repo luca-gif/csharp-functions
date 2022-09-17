8427dc2b0a40dbeffcbef99ac7e381e8ef9d3fec
luca-gif <luca.89m@gmail.com>
2022-09-17 17:27:34 +0200

    Bonus

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@
 //void StampaArray(int[] array): che preso un array di numeri interi e
 //stampa a video il contenuto dell’array in questa forma “[elemento 1, elemento 2, elemento 3, ...]“.
 
+using System;
+
 int[] numbers = {1,2,3,4,5};
 
 
@@ -71,18 +73,41 @@ int[] arrNumbers = { 2, 6, 7, 5, 3, 9 };
 
 Console.WriteLine("Stampare l’array di numeri fornito a video");
 StampaArray(arrNumbers);
-
+Console.WriteLine("");
 
 Console.WriteLine("");
 Console.WriteLine("Stampare l’array di numeri fornito a video, dove ogni numero è stato prima elevato al quadrato.");
 StampaArray(ElevaArrayAlQuadrato(arrNumbers));
-
+Console.WriteLine("");
 
 Console.WriteLine("");
 Console.WriteLine("Stampare la somma di tutti i numeri");
 Console.WriteLine(sommaElementiArray(arrNumbers));
 
-
 Console.WriteLine("");
 Console.WriteLine("Stampare la somma di tutti i numeri elevati al quadrati.");
 Console.WriteLine(sommaElementiArray(ElevaArrayAlQuadrato(arrNumbers)));
+
+
+//BONUS
+
+/*Convertire le funzioni appena dichiarate in funzioni generiche, 
+ossia funzioni che possono lavorare con array di numeri interi di lunghezza variabile, 
+ossia debbono poter funzionare sia che gli passi array di 5 elementi, sia di 6, di 7, di ... e così via. 
+A questo punto modificare il programma in modo che chieda all’utente quanti numeri voglia inserire, 
+e dopo di che questi vengono inseriti a mano dall’utente esternamente.
+Rieseguire il programma con l’input preso esternamente dall’utente.*/
+
+Console.WriteLine("Quanti numeri vuoi inserire?");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] arrayNum = new int[number];
+
+for(int i=0; i<number; i++)
+{
+    Console.WriteLine($"Inserisci il {i + 1}° numero");
+    arrayNum[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+StampaArray(arrayNum);
