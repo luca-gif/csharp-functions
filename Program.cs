c4cdc1f889a838efa2fb9748116ca6348581f308
luca-gif <luca.89m@gmail.com>
2022-09-17 15:03:06 +0200

    esercizio 2

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//FUNZIONE 1
 
 //void StampaArray(int[] array): che preso un array di numeri interi e
 //stampa a video il contenuto dell’array in questa forma “[elemento 1, elemento 2, elemento 3, ...]“.
-
+/*
 int[] numbers = {1,2,3,4,5};
 
 
@@ -18,4 +18,27 @@ void StampaArray(int[] array)
     Console.Write("]");
 }
 
-StampaArray(numbers);
+StampaArray(numbers);*/
+
+
+//FUNZIONE 2
+
+//int Quadrato(int numero): che vi restituisca il quadrato del numero passato come parametro.
+
+/*int Quadrato(int numero)
+{
+    return numero * numero;
+    
+}
+
+Console.WriteLine(Quadrato(10));*/
+
+//FUNZIONE 3
+/* int[] ElevaArrayAlQuadrato(int[] array): che preso un array di numeri interi, restituisca un nuovo array con tutti gli elementi
+elevati quadrato. Attenzione: è importante restituire un nuovo array, e non modificare l’array come parametro della funzione!
+Vi ricordate perchè? Pensateci (vedi slide) */
+
+int[] ElevaArrayAlQuadrato(int[] array)
+{
+
+}
